be682a1c1db34d5c425f736580364e4f5abdbd6b
Alejandrodlk <ale_165851@hotmail.com>
2022-03-13 01:56:57 -0300

    Servidor corriendo

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+const port = 3030
+
+app.use(express.static("public"))
+
+
+app.get("/" , (req,res) => res.sendFile(path.join(__dirname , "/views/index.html")))
+app.get("/about" , (req,res) => res.sendFile(path.join(__dirname , "/views/about.html")))
+app.get("/contact" , (req,res) => res.sendFile(path.join(__dirname , "/views/contact.html")))
+app.get("/music" , (req,res) => res.sendFile(path.join(__dirname , "/views/music.html")))
+app.get("*" , (req,res) => res.sendFile(path.join(__dirname , "/views/notFound.html")))
+
+app.listen(port , () => console.log(`Servidor corriendo en el puerto http://localhost:${port}`))
